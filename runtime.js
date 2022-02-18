f764ce3217f03949b0d0ffa5b7b7ad9dc8bb22cf
juanskysword <juanskysword@gmail.com>
2022-02-18 12:08:50 -0600

    finish

--- runtime.js
+++ runtime.js
@@ -48,15 +48,87 @@ const extraLargeArray = getSizedArray(100000);
 // Try it with first function
 perf.start();                     // Starts timer
 doublerAppend(tinyArray);
-let resultsAppend = perf.stop();  // Stops timer and save time results
+let tinyArrayresultsAppend = perf.stop();  // Stops timer and save time results
 
 
 // Try it with second function
 perf.start();
 doublerInsert(tinyArray);
-let resultsInsert = perf.stop();
+let tinyArrayresultsInsert = perf.stop();
+
+
+console.log('Results for the tinyArray');
+console.log("insert", tinyArrayresultsInsert.preciseWords);
+console.log("append", tinyArrayresultsAppend.preciseWords);
+console.log('-------------------------------')
+//--------------------------------------------------------------------
+
+//first function
+perf.start();                     
+doublerAppend(smallArray);
+let smallArrayresultsAppend = perf.stop();  
+
+
+//second function
+perf.start();
+doublerInsert(smallArray);
+let smallArrayresultsInsert = perf.stop();
+
+
+console.log('Results for the smallArray');
+console.log("insert", smallArrayresultsInsert.preciseWords);
+console.log("append", smallArrayresultsAppend.preciseWords);
+console.log('-------------------------------')
+//--------------------------------------------------------------------
+
+//first function
+perf.start();                     
+doublerAppend(mediumArray);
+let mediumArrayresultsAppend = perf.stop();  
+
+
+//second function
+perf.start();
+doublerInsert(mediumArray);
+let mediumArrayresultsInsert = perf.stop();
+
+
+console.log('Results for the mediumArray');
+console.log("insert", mediumArrayresultsInsert.preciseWords);
+console.log("append", mediumArrayresultsAppend.preciseWords);
+console.log('-------------------------------')
+//--------------------------------------------------------------------
+
+//first function
+perf.start();                     
+doublerAppend(largeArray);
+let largeArrayresultsAppend = perf.stop();  
+
+
+//second function
+perf.start();
+doublerInsert(largeArray);
+let largeArrayresultsInsert = perf.stop();
+
+
+console.log('Results for the largeArray');
+console.log("insert", largeArrayresultsInsert.preciseWords);
+console.log("append", largeArrayresultsAppend.preciseWords);
+console.log('-------------------------------')
+//--------------------------------------------------------------------
+
+//first function
+perf.start();                     
+doublerAppend(extraLargeArray);
+let extraLargeArrayresultsAppend = perf.stop();  
+
+
+//second function
+perf.start();
+doublerInsert(extraLargeArray);
+let extraLargeArrayresultsInsert = perf.stop();
 
 
 console.log('Results for the extraLargeArray');
-console.log("insert", resultsInsert.preciseWords);
-console.log("append", resultsAppend.preciseWords);
+console.log("insert", extraLargeArrayresultsInsert.preciseWords);
+console.log("append", extraLargeArrayresultsAppend.preciseWords);
